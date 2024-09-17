8011857cf63d3bc7cb63dec596774e6d0cae1535
Daniel Kuzminski <dankuzmina@gmail.com>
2024-09-17 15:25:59 +0200

    create intro section, add desktop and mobile styles

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 //components
 import Hero from './components/Hero';
+import Intro from './components/Intro';
 
 function App() {
   return (
     <div className="App">
       <Hero />
+      <Intro />
     </div>
   );
 }
---
+++ src/components/Intro.jsx
@@ -0,0 +1,23 @@
+//styles
+import "./intro.css"
+
+export default function Intro() {
+	return (
+		<section className="introduction">
+			<div className="side-a"></div>
+			<div className="side-b">
+				<h2 className="introduction-title">Discover the Iconic Mini Cooper</h2>
+				<p className="description">
+					Unlock the extraordinary with the{" "}
+					<span className="orange">Mini Cooper</span> – a timeless icon of style,
+					performance, and driving pleasure.
+				</p>
+				<p className="description">
+					Crafted with precision engineering and infused with decades of
+					innovation, the Mini Cooper is more than just a car; it's a lifestyle
+					statement, an embodiment of freedom on the road.
+				</p>
+			</div>
+		</section>
+	)
+}
