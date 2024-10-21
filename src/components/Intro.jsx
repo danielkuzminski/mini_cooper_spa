bc154f380c58a3c4c42995b3349157b0951ee45e
Daniel Kuzminski <dankuzmina@gmail.com>
2024-10-21 15:56:48 +0200

    recreate intro section

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,23 +1,37 @@
 //styles
 import "./intro.css"
 
+//assets
+
+
 export default function Intro() {
 	return (
-		<section className="introduction">
-			<div className="side-a"></div>
-			<div className="side-b">
-				<h2 className="introduction-title">Discover the Iconic Mini Cooper</h2>
-				<p className="description">
-					Unlock the extraordinary with the{" "}
-					<span className="orange">Mini Cooper</span> – a timeless icon of style,
-					performance, and driving pleasure.
+		<section className="intro">
+			<div className="intro-left intro-container">
+				<h2 className="intro-title">
+					Discover the Iconic Mini
+				</h2>
+				<img width="500" height="200" src="" alt="" />
+				<h3 className="intro-subtitle">
+					Unlock the extraordinary
+				</h3>
+				<p className="intro-text">
+					A timeless icon of style, performance, and driving pleasure.
 				</p>
-				<p className="description">
-					Crafted with precision engineering and infused with decades of
-					innovation, the Mini Cooper is more than just a car... It's a <span className="orange">lifestyle
-					statement</span>, an embodiment of freedom on the road.
+				<p className="intro-text">
+					Crafted with precision engineering and infused with decades of innovation, the Mini Cooper is more than just a car... It's a lifestyle statement
 				</p>
+			</div>
+			<div className="intro-right intro-container">
+				<h4 className="mini-container">
+					<span className="mini">mini</span>
+				</h4>
+				<p className="intro-car">cooper</p>
+				<p className="intro-car">one</p>
+				<p className="intro-car">clubman</p>
+				<p className="intro-car">countryman</p>
 			</div>
 		</section>
 	)
 }
+
